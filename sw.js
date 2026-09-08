b013b7eeffaf72346c408bde1d91a97e9be62987
Corahuertacoach <adhrslp@gmail.com>
2026-09-07 18:01:08 -0600

    Update service worker caching logic and variable names

--- sw.js
+++ sw.js
@@ -1,31 +1,14 @@
-const CACHE_NAME = "mizonadepoder-v1";
-const APP_SHELL = ["/", "/index.html"];
+const CACHE_NAME = 'mzdp-cache-v1';
+const urlsToCache = ['/', '/index.html'];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request)
-      .then((res) => {
-        const resClone = res.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
-        return res;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
